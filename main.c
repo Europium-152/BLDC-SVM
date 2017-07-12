5bcdca3e661ee8c649dd6e1aa9dfd7f8eff592e2
Europium-152 <daniel.hfc96@gmail.com>
2017-07-12 16:07:45 +0100

    PWM_init
    Só um teste em updown count mode com os 3 PWM com o mesmo duty cycle center aligned

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
-#define FCY 29491200L
+#define FCY 4*7372800   //instruction frequency in Hz
+#define FOSC 7372800    //crystal vibration frequency in Hz
+#include <p30F4011.h>   //defines dos dspic registers
+#include <stdio.h>	    //standart IO library C
+#include <string.h>     //string related functions library
+#include <libpic30.h>	//C30 compiler definitions
+#include <xc.h>         //Compiler
 
-#include <stdio.h>
+void PWM_init();
 
-ahaha
+int main(void) {
+    
+    PWM_init();
+    
+    
+    return 0;
+}
+void PWM_init(){
+
+    PTCONbits.PTMOD=0b10;
+    PTPERbits.PTPER=32700; //max 32767
+    PDC1=32700/2;
+    PDC2=32700/2;
+    PDC3=PDC1;
+    
+}
