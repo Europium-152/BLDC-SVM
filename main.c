27bab320edd00ad50a36a456c5f149deb74286dd
WildRC <valentim.ruben@gmail.com>
2017-07-12 11:58:54 +0100

    initial definitions

--- main.c
+++ main.c
@@ -4,3 +4,11 @@
 #include <libpic30.h>
 #include <p30F4011.h>
 #include <stdio.h>
+
+/* For XC16 Ver >= 1.24 */
+#pragma config FCKSMEN=CSW_FSCM_OFF
+#pragma config FOS=PRI
+#pragma config FPR=XT_PLL16 
+#pragma config WDT=WDT_OFF
+#pragma config MCLRE=MCLR_EN
+#pragma config FPWRT=PWRT_OFF
