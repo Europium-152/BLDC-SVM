9b6c798f1ead38c3aa959888a57579b95a4fa3e5
WildRC <valentim.ruben@gmail.com>
2017-07-13 17:13:27 +0100

    g_m update

--- main.c
+++ main.c
@@ -16,35 +16,35 @@ void setPDC(int setor, int angB) {
 	//switch
 	    switch(setor){
 	        case 1:
-				PDC1 = 2*MY_PTPER - t02; //Phase R duty cycle T0/2
-				PDC2 = tB + t02;//Phase Y duty cycle T0/2+TA
-				PDC3 = t02;//Phase B duty cycle TS-T0/2
+				PDC1 = g_m*2*MY_PTPER - t02; //Phase R duty cycle T0/2
+				PDC2 = g_m*tB + t02;//Phase Y duty cycle T0/2+TA
+				PDC3 = g_m*t02;//Phase B duty cycle TS-T0/2
 	            break;
 	        case 2:    
-				PDC1 = t02 + tA;//Phase R duty cycle T0/2+TB
-				PDC2 = 2*MY_PTPER - t02;//Phase Y duty cycle T0/2
-				PDC3 = t02;//Phase B duty cycle TS-T0/2    
+				PDC1 = g_m*t02 + tA;//Phase R duty cycle T0/2+TB
+				PDC2 = g_m*2*MY_PTPER - t02;//Phase Y duty cycle T0/2
+				PDC3 = g_m*t02;//Phase B duty le TS-T0/2    
 	            break;
 	        case 3:
-				PDC1 = t02;//Phase R duty cycle TS-T0/2
-				PDC2 = 2*MY_PTPER - t02;//Phase Y duty cycle T0/2
-				PDC3 = t02 + tB; //Phase B duty cycle T0/2+TA
+				PDC1 = g_m*t02;//Phase R duty cycle TS-T0/2
+				PDC2 = g_m*2*MY_PTPER - t02;//Phase Y duty cycle T0/2
+				PDC3 = g_m*t02 + tB; //Phase B duty cycle T0/2+TA
 	            break;
 	        case 4:
-				PDC1 = t02;//Phase R duty cycle TS-T0/2
-				PDC2 = tA + t02;//Phase Y duty cycle T0/2+TB
-				PDC3 = 2*MY_PTPER - t02;//Phase B duty cycle T0/2
+				PDC1 = g_m*t02;//Phase R duty cycle TS-T0/2
+				PDC2 = g_m*tA + t02;//Phase Y duty cycle T0/2+TB
+				PDC3 = g_m*2*MY_PTPER - t02;//Phase B duty cycle T0/2
 	            break;
 	        case 5:
-				PDC1 = t02 + tA;//Phase R duty cycle T0/2+TA
-				PDC2 = t02;//Phase Y duty cycle TS-T0/2
-				PDC3 = 2*MY_PTPER - t02;//Phase B duty cycle T0/2
+				PDC1 = g_m*t02 + tB;//Phase R duty cycle T0/2+TA
+				PDC2 = g_m*t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = g_m*2*MY_PTPER - t02;//Phase B duty cycle T0/2
 	            break;
-	        case 6:   
-				PDC1 = 2*MY_PTPER - t02;//Phase R duty cycle T0/2
-				PDC2 = t02;//Phase Y duty cycle TS-T0/2
-				PDC3 = t02 + tA;//Phase B duty cycle T0/2+TB
-	            break;				
+	        case 6:
+				PDC1 = g_m*2*MY_PTPER - t02;//Phase R duty cycle T0/2
+				PDC2 = g_m*t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = g_m*t02 + tA;//Phase B duty cycle T0/2+TB
+	            break;							
 	  }
 
 }
