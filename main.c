a81590c5a9f3ec7221da8b25ecc65ae78936d505
Europium-152 <daniel.hfc96@gmail.com>
2017-07-13 17:26:15 +0100

    Versão Estável
    Com resistências de 3,3 Ohm em cada enrolamento

--- main.c
+++ main.c
@@ -25,6 +25,8 @@
 
 int g_ang=0, g_setor=1;
 
+float g_m=0.5;
+
 int tab[20] = {0, 43, 86, 128, 170, 212, 253, 293, 333, 371, 409, 446, 481, 515, 547, 578, 608, 636, 662, 686};
 
 void setPDC(int setor, int angB);
@@ -49,7 +51,7 @@ int main(void) {
 void PWM_init(){
 
     PTCONbits.PTMOD=0b10;  //Working mode up-down counting 0b10
-    PTCONbits.PTCKPS=0b10; //Source-Timer Prescaler (0,1,2,3) (1:1, 1:4, 1:16, 1:64)
+    PTCONbits.PTCKPS=0b00; //Source-Timer Prescaler (0,1,2,3) (1:1, 1:4, 1:16, 1:64)
     PTPER=818;             //max 32767
     PTMR=0;                //Set the timer to initial value 0
     PDC1=PTPER;            //if PDC is PTPER dc=50%, PDC is double of duty-cycle
@@ -83,7 +85,7 @@ void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void )
     //your code here
     IFS2bits.PWMIF = 0; //Clear Interrupt Flag
     static int i=0;
-    if(i==350)
+    if(i==3)
     { //Update PDC code
         setPDC(g_setor,g_ang);
         g_ang++;
@@ -115,37 +117,37 @@ void setPDC(int setor, int angB) {
 	t02 = (2*MY_PTPER - tA - tB)/2;
 
 	//switch
-	    switch(setor){
+	       switch(setor){
 	        case 1:
-				PDC1 = 2*MY_PTPER - t02; //Phase R duty cycle T0/2
-				PDC2 = tB + t02;//Phase Y duty cycle T0/2+TA
-				PDC3 = t02;//Phase B duty cycle TS-T0/2
+				PDC1 = g_m*(2*MY_PTPER - t02); //Phase R duty cycle T0/2
+				PDC2 = g_m*(tB + t02);//Phase Y duty cycle T0/2+TA
+				PDC3 = g_m*t02;//Phase B duty cycle TS-T0/2
 	            break;
 	        case 2:    
-				PDC1 = t02 + tA;//Phase R duty cycle T0/2+TB
-				PDC2 = 2*MY_PTPER - t02;//Phase Y duty cycle T0/2
-				PDC3 = t02;//Phase B duty cycle TS-T0/2    
+				PDC1 = g_m*(t02 + tA);//Phase R duty cycle T0/2+TB
+				PDC2 = g_m*(2*MY_PTPER - t02);//Phase Y duty cycle T0/2
+				PDC3 = g_m*t02;//Phase B duty cycle TS-T0/2    
 	            break;
 	        case 3:
-				PDC1 = t02;//Phase R duty cycle TS-T0/2
-				PDC2 = 2*MY_PTPER - t02;//Phase Y duty cycle T0/2
-				PDC3 = t02 + tB; //Phase B duty cycle T0/2+TA
+				PDC1 = g_m*t02;//Phase R duty cycle TS-T0/2
+				PDC2 = g_m*(2*MY_PTPER - t02);//Phase Y duty cycle T0/2
+				PDC3 = g_m*(t02 + tB); //Phase B duty cycle T0/2+TA
 	            break;
 	        case 4:
-				PDC1 = t02;//Phase R duty cycle TS-T0/2
-				PDC2 = tA + t02;//Phase Y duty cycle T0/2+TB
-				PDC3 = 2*MY_PTPER - t02;//Phase B duty cycle T0/2
+				PDC1 = g_m*t02;//Phase R duty cycle TS-T0/2
+				PDC2 = g_m*(tA + t02);//Phase Y duty cycle T0/2+TB
+				PDC3 = g_m*(2*MY_PTPER - t02);//Phase B duty cycle T0/2
 	            break;
 	        case 5:
-				PDC1 = t02 + tB;//Phase R duty cycle T0/2+TA
-				PDC2 = t02;//Phase Y duty cycle TS-T0/2
-				PDC3 = 2*MY_PTPER - t02;//Phase B duty cycle T0/2
+				PDC1 = g_m*(t02 + tB);//Phase R duty cycle T0/2+TA
+				PDC2 = g_m*t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = g_m*(2*MY_PTPER - t02);//Phase B duty cycle T0/2
 	            break;
-	        case 6:   
-				PDC1 = 2*MY_PTPER - t02;//Phase R duty cycle T0/2
-				PDC2 = t02;//Phase Y duty cycle TS-T0/2
-				PDC3 = t02 + tA;//Phase B duty cycle T0/2+TB
-	            break;				
+	        case 6:
+				PDC1 = g_m*(2*MY_PTPER - t02);//Phase R duty cycle T0/2
+				PDC2 = g_m*t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = g_m*(t02 + tA);//Phase B duty cycle T0/2+TB
+	            break;							
 	  }
 
 }
