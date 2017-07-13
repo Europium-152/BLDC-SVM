c7c2472b668aad0a6e91c3818ef7e0e2c71d5444
Europium-152 <daniel.hfc96@gmail.com>
2017-07-13 16:23:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #define FCY 29491200L
 
+#define MY_PTPER 818
+
 /* Include List */
 #include <libpic30.h>
 #include <p30F4011.h>
@@ -19,6 +21,12 @@
 #pragma config WDT=WDT_OFF
 #pragma config MCLRE=MCLR_EN
 #pragma config FPWRT=PWRT_OFF
+
+int g_ang=0, g_setor=0;
+
+int tab[20] = {0, 43, 86, 128, 170, 212, 253, 293, 333, 371, 409, 446, 481, 515, 547, 578, 608, 636, 662, 686};
+
+void setPDC(int setor, int angB);
 
 void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void );
 
@@ -75,7 +83,19 @@ void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void )
     IFS2bits.PWMIF = 0; //Clear Interrupt Flag
     static int i=0;
     if(i==3)
-    {
+    { //Update PDC code
+        setPDC(g_setor,g_ang);
+        g_ang++;
+        if(g_ang==20){
+            g_setor++;
+            g_ang=0;
+            if(g_setor==7)
+                g_setor=1;
+        }
+               
+           
+        
+        
        _LATF0=(!_LATF0); //Every 3 interrupts toggle F0 bit, which connects to LED 26
        i=0;
     }
@@ -83,4 +103,48 @@ void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void )
     
     
     
+}
+
+
+void setPDC(int setor, int angB) {
+	static int tA, tB, t02, angA;
+	angA = 19 - angB;
+	tB = 2 * tab[angB];
+	tA = 2 * tab[angA];
+	t02 = (2*MY_PTPER - tA - tB)/2;
+
+	//switch
+	    switch(setor){
+	        case 1:
+				PDC1 = 2*MY_PTPER - t02; //Phase R duty cycle T0/2
+				PDC2 = tB + t02;//Phase Y duty cycle T0/2+TA
+				PDC3 = t02;//Phase B duty cycle TS-T0/2
+	            break;
+	        case 2:    
+				PDC1 = t02 + tA;//Phase R duty cycle T0/2+TB
+				PDC2 = 2*MY_PTPER - t02;//Phase Y duty cycle T0/2
+				PDC3 = t02;//Phase B duty cycle TS-T0/2    
+	            break;
+	        case 3:
+				PDC1 = t02;//Phase R duty cycle TS-T0/2
+				PDC2 = 2*MY_PTPER - t02;//Phase Y duty cycle T0/2
+				PDC3 = t02 + tB; //Phase B duty cycle T0/2+TA
+	            break;
+	        case 4:
+				PDC1 = t02;//Phase R duty cycle TS-T0/2
+				PDC2 = tA + t02;//Phase Y duty cycle T0/2+TB
+				PDC3 = 2*MY_PTPER - t02;//Phase B duty cycle T0/2
+	            break;
+	        case 5:
+				PDC1 = t02 + tA;//Phase R duty cycle T0/2+TA
+				PDC2 = t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = 2*MY_PTPER - t02;//Phase B duty cycle T0/2
+	            break;
+	        case 6:   
+				PDC1 = 2*MY_PTPER - t02;//Phase R duty cycle T0/2
+				PDC2 = t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = t02 + tA;//Phase B duty cycle T0/2+TB
+	            break;				
+	  }
+
 }
