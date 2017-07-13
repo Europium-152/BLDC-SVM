fead85714faae214899fabb052ddb4138416ec84
Europium-152 <daniel.hfc96@gmail.com>
2017-07-13 12:25:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,69 +1,86 @@
-#define FCY 4*7372800   //instruction frequency in Hz
-#define FOSC 7372800    //crystal vibration frequency in Hz
-#include <p30F4011.h>   //defines dos dspic registers
-#include <stdio.h>	    //standart IO library C
-#include <string.h>     //string related functions library
-#include <libpic30.h>	//C30 compiler definitions
-#include <xc.h>         //Compiler
+#define FCY 29491200L
+
+/* Include List */
+#include <libpic30.h>
+#include <p30F4011.h>
+#include <stdio.h>
+
+/* Global Definitions */
+
+/* For XC16 Ver <= 1.24 */
+//_FOSC(CSW_FSCM_OFF & XT_PLL16); //oscillator at 16x crystal frequency
+//_FWDT(WDT_OFF);                 //watchdog timer off
+//_FBORPOR(MCLR_EN & PWRT_OFF);   //Turn MCLR pin ON and Energy Timers OFF
+
+/* For XC16 Ver >= 1.24 */
+#pragma config FCKSMEN=CSW_FSCM_OFF
+#pragma config FOS=PRI
+#pragma config FPR=XT_PLL16 
+#pragma config WDT=WDT_OFF
+#pragma config MCLRE=MCLR_EN
+#pragma config FPWRT=PWRT_OFF
+
+void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void );
 
 void PWM_init();
 
 int main(void) {
     
+    _TRISF0 = 0;
+
+    
     PWM_init();
-    
-    //    switch(setor){
-//        case 1:
-//            //Phase R duty cycle T0/2
-//            //Phase Y duty cycle T0/2+TA
-//            //Phase B duty cycle TS-T0/2
-//            break;
-//        case 2:    
-//            //Phase R duty cycle T0/2+TB
-//            //Phase Y duty cycle T0/2
-//            //Phase B duty cycle TS-T0/2    
-//            break;
-//        case 3:
-//            //Phase R duty cycle TS-T0/2
-//            //Phase Y duty cycle T0/2
-//            //Phase B duty cycle T0/2+TA
-//            break;
-//        case 4:
-//            //Phase R duty cycle TS-T0/2
-//            //Phase Y duty cycle T0/2+TB
-//            //Phase B duty cycle T0/2
-//            break;
-//        case 5:
-//            //Phase R duty cycle T0/2+TA
-//            //Phase Y duty cycle TS-T0/2
-//            //Phase B duty cycle T0/2
-//            break;
-//        case 6:   
-//            //Phase R duty cycle T0/2
-//            //Phase Y duty cycle TS-T0/2
-//            //Phase B duty cycle T0/2+TB
-//            break;
-//        default:
-//            return -1;
-                    
-            
- //   }
-    
+        
     while(1);
     
     return 0;
 }
+
+//Scale 1:1 e PTPER 818 => 18kHz pwm
 void PWM_init(){
 
     PTCONbits.PTMOD=0b10;  //Working mode up-down counting 0b10
-    PTCONbits.PTCKPS=0b01; //Source-Timer Prescaler (0,1,2,3) (1:1, 1:4, 1:16, 1:64)
-    PTPER=1000; //max 32767
-    PTMR=0;     //Set the timer to incial value 0
-    PDC1=500*2; //PDC is double of duty-cycle
-    //PDC2=32700/4;
-    //PDC3=PDC1;
-    PWMCON1bits.PEN1H=1;
+    PTCONbits.PTCKPS=0b00; //Source-Timer Prescaler (0,1,2,3) (1:1, 1:4, 1:16, 1:64)
+    PTPER=818;             //max 32767
+    PTMR=0;                //Set the timer to initial value 0
+    PDC1=PTPER;            //if PDC is PTPER dc=50%, PDC is double of duty-cycle
+    PDC2=PTPER/2;          //dc=25%
+    PDC3=PTPER+PTPER/2;    //dc=75%
+    PWMCON1bits.PEN1H=1;   //Enable PWMxH pins
+    PWMCON1bits.PEN2H=1;
+    PWMCON1bits.PEN3H=1;
     
-    PTCONbits.PTEN=1;
+    PWMCON1bits.PEN1L=0;   //Disable PWMxL pins, use as I/O for enabling L6234
+    PWMCON1bits.PEN2L=0;
+    PWMCON1bits.PEN3L=0;
+    
+    _TRISE2 = 0;
+    _TRISE0 = 0;
+    _TRISE4 = 0;
+    
+    _LATE2 = 1;
+    _LATE0 = 1;
+    _LATE4 = 1;        
+    
+    IEC2bits.PWMIE=1;      //Interrupt Enable, Flag is IFS2bits.PWMIF
+    
+    
+    PTCONbits.PTEN=1;      //Enable PWM at end of Initiation routine
+    
+}
+
+void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void )               
+{
+    //your code here
+    IFS2bits.PWMIF = 0; //Clear Interrupt Flag
+    static int i=0;
+    if(i==3)
+    {
+       _LATF0=(!_LATF0); //Every 3 interrupts toggle F0 bit, which connects to LED 26
+       i=0;
+    }
+    i++;
+    
+    
     
 }
