ab29e4dcbb0dfadd0202ef2cf652acc33ec374c8
Europium-152 <daniel.hfc96@gmail.com>
2017-07-14 10:28:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 
 #define FCY 29491200L
 
-#define MY_PTPER 818
+#define MY_PTPER 818          //max 32767
+#define LAST_TABLE_INDEX 19
+#define ANGLE_RESOLUTION 20
 
 /* Include List */
 #include <libpic30.h>
@@ -23,10 +25,17 @@
 #pragma config MCLRE=MCLR_EN
 #pragma config FPWRT=PWRT_OFF
 
-int g_ang=0, g_setor=1;
+//Global Variables
+int g_ang=0,           //Converted angle, stores the angle in units of its resolution, 
+                       //Working with 128 resolution, the total 360 circle is 128*60=7680 units long
+    g_atCount=3,       //Actualization count g_atCount=x means duty cycles get actualized every 3 PWM periods
+    g_angIncrement=1,  //angle increment step Magnetic Fiel freq. is FPWM/atCount*(angIncrement/7680)    
+    g_sector=0,        //circle sector 0 to 5
+    g_m=2;             //VDC divisor, g_m=x means max tension is equal to V_unreg/x
+    
 
-float g_m=0.5;
-
+     
+//Lookup table for sin(g_angle)*PTPER
 int tab[20] = {0, 43, 86, 128, 170, 212, 253, 293, 333, 371, 409, 446, 481, 515, 547, 578, 608, 636, 662, 686};
 
 void setPDC(int setor, int angB);
@@ -47,12 +56,12 @@ int main(void) {
     return 0;
 }
 
-//Scale 1:1 e PTPER 818 => 18kHz pwm
+//Scale 1:1 e PTPER 589 => 25kHz pwm
 void PWM_init(){
 
     PTCONbits.PTMOD=0b10;  //Working mode up-down counting 0b10
     PTCONbits.PTCKPS=0b00; //Source-Timer Prescaler (0,1,2,3) (1:1, 1:4, 1:16, 1:64)
-    PTPER=818;             //max 32767
+    PTPER=MY_PTPER;        //max 32767
     PTMR=0;                //Set the timer to initial value 0
     PDC1=PTPER;            //if PDC is PTPER dc=50%, PDC is double of duty-cycle
     PDC2=PTPER/2;          //dc=25%
@@ -85,21 +94,16 @@ void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void )
     //your code here
     IFS2bits.PWMIF = 0; //Clear Interrupt Flag
     static int i=0;
-    if(i==3)
+    static int tempAng=0;
+    
+    if(i==g_atCount)
     { //Update PDC code
-        setPDC(g_setor,g_ang);
-        g_ang++;
-        if(g_ang==20){
-            g_setor++;
-            g_ang=0;
-            if(g_setor==7)
-                g_setor=1;
-        }
-               
-           
+        tempAng=g_ang+g_angIncrement;
+        setPDC(g_sector,g_ang);
+        g_ang=tempAng%ANGLE_RESOLUTION;
+        g_sector=(g_sector+tempAng/ANGLE_RESOLUTION)%6;     
         
-        
-       _LATF0=(!_LATF0); //Every 3 interrupts toggle F0 bit, which connects to LED 26
+       _LATF0=(!_LATF0); //Every g_atCount interrupts toggle F0 bit, which connects to LED D6
        i=0;
     }
     i++;
@@ -111,43 +115,43 @@ void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void )
 
 void setPDC(int setor, int angB) {
 	static int tA, tB, t02, angA;
-	angA = 19 - angB;
-	tB = 2 * tab[angB];
-	tA = 2 * tab[angA];
+	angA = LAST_TABLE_INDEX - angB;
+	tB = 2 * tab[angB]/g_m;
+	tA = 2 * tab[angA]/g_m;
 	t02 = (2*MY_PTPER - tA - tB)/2;
 
 	//switch
 	       switch(setor){
-	        case 1:
-				PDC1 = g_m*(2*MY_PTPER - t02); //Phase R duty cycle T0/2
-				PDC2 = g_m*(tB + t02);//Phase Y duty cycle T0/2+TA
-				PDC3 = g_m*t02;//Phase B duty cycle TS-T0/2
+	        case 0:
+				PDC1 = (2*MY_PTPER - t02); //Phase R duty cycle T0/2
+				PDC2 = (tB + t02);//Phase Y duty cycle T0/2+TA
+				PDC3 = t02;//Phase B duty cycle TS-T0/2
 	            break;
-	        case 2:    
-				PDC1 = g_m*(t02 + tA);//Phase R duty cycle T0/2+TB
-				PDC2 = g_m*(2*MY_PTPER - t02);//Phase Y duty cycle T0/2
-				PDC3 = g_m*t02;//Phase B duty cycle TS-T0/2    
+	        case 1:    
+				PDC1 = (t02 + tA);//Phase R duty cycle T0/2+TB
+				PDC2 = (2*MY_PTPER - t02);//Phase Y duty cycle T0/2
+				PDC3 = t02;//Phase B duty cycle TS-T0/2    
+	            break;
+	        case 2:
+				PDC1 = t02;//Phase R duty cycle TS-T0/2
+				PDC2 = (2*MY_PTPER - t02);//Phase Y duty cycle T0/2
+				PDC3 = (t02 + tB); //Phase B duty cycle T0/2+TA
 	            break;
 	        case 3:
-				PDC1 = g_m*t02;//Phase R duty cycle TS-T0/2
-				PDC2 = g_m*(2*MY_PTPER - t02);//Phase Y duty cycle T0/2
-				PDC3 = g_m*(t02 + tB); //Phase B duty cycle T0/2+TA
+				PDC1 = t02;//Phase R duty cycle TS-T0/2
+				PDC2 = (tA + t02);//Phase Y duty cycle T0/2+TB
+				PDC3 = (2*MY_PTPER - t02);//Phase B duty cycle T0/2
 	            break;
 	        case 4:
-				PDC1 = g_m*t02;//Phase R duty cycle TS-T0/2
-				PDC2 = g_m*(tA + t02);//Phase Y duty cycle T0/2+TB
-				PDC3 = g_m*(2*MY_PTPER - t02);//Phase B duty cycle T0/2
+				PDC1 = (t02 + tB);//Phase R duty cycle T0/2+TA
+				PDC2 = t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = (2*MY_PTPER - t02);//Phase B duty cycle T0/2
 	            break;
 	        case 5:
-				PDC1 = g_m*(t02 + tB);//Phase R duty cycle T0/2+TA
-				PDC2 = g_m*t02;//Phase Y duty cycle TS-T0/2
-				PDC3 = g_m*(2*MY_PTPER - t02);//Phase B duty cycle T0/2
-	            break;
-	        case 6:
-				PDC1 = g_m*(2*MY_PTPER - t02);//Phase R duty cycle T0/2
-				PDC2 = g_m*t02;//Phase Y duty cycle TS-T0/2
-				PDC3 = g_m*(t02 + tA);//Phase B duty cycle T0/2+TB
-	            break;							
+				PDC1 = (2*MY_PTPER - t02);//Phase R duty cycle T0/2
+				PDC2 = t02;//Phase Y duty cycle TS-T0/2
+				PDC3 = (t02 + tA);//Phase B duty cycle T0/2+TB
+	            //break;							
 	  }
 
 }
