0dcb7edf45215674cb2369930e0abc0e9b3dc550
Europium-152 <daniel.hfc96@gmail.com>
2017-07-20 11:58:05 +0100

    versao para chegar aos 7200 rpms

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 
-
 #define FCY 29491200L
 
 #define MY_PTPER 589          //max 32767
 #define LAST_TABLE_INDEX 127
 #define ANGLE_RESOLUTION 128
+#define PR2_INITIAL_VALUE 65000
+#define PR2_FINAL_VALUE 639
 
 #define MY_PR3 30000
-#define INVERSE_FACTOR 147456.
+#define INVERSE_FACTOR 4718430.
+#define NEXT_INVERSE_FACTOR 301980000.
+#define ACELERACAO 1.8 //Aceleracao do campo girante em  Hz/s
+
+#define GM_FIRST_JUMP 10
+#define GM_SECOND_JUMP 6
 
 /* Include List */
 #include <libpic30.h>
@@ -29,14 +35,15 @@
 #pragma config MCLRE=MCLR_EN
 #pragma config FPWRT=PWRT_OFF
 
-float g_inverse=INVERSE_FACTOR;
+float g_inverse=INVERSE_FACTOR/ACELERACAO, 
+      g_truePR2=PR2_INITIAL_VALUE;
 
 //Global Variables
 int g_ang=0,           //Converted angle, stores the angle in units of its resolution, 
                        //Working with 128 resolution, the total 360 circle is 128*60=7680 units long
-    g_angIncrement=1,  //angle increment step Magnetic Fiel freq. is FPWM/atCount*(angIncrement/7680)    
-    g_sector=0,        //circle sector 0 to 5
-    g_m=2;             //VDC divisor, g_m=x means max tension is equal to V_unreg/x
+    g_angIncrement=8,  //angle increment step Magnetic Fiel freq. is FPWM/atCount*(angIncrement/7680)    
+    g_sector=0,       //circle sector 0 to 5
+    g_m=5;             //VDC divisor, g_m=x means max tension is equal to V_unreg/x
     
 
      
@@ -62,13 +69,17 @@ void TIMER2_config();
 
 void TIMER3_config();
 
+void start_breaking();
+
+void regen();
+
 int main(void) {
     
     _TRISF0 = 0;
     _TRISF1 = 0;
     _TRISD3 = 0;
 
-    _LATF0 = 0;
+    _LATF0 = 1;
     _LATF1 = 0;
     _LATD3 = 0;
     
@@ -77,6 +88,8 @@ int main(void) {
     TIMER2_config();
     TIMER3_config();
         
+   
+    
     while(1);
     
     return 0;
@@ -86,12 +99,13 @@ void TIMER2_config(){
     
     T2CONbits.TON = 0;      //Timer_2 is OFF
     TMR2 = 0;               //resets Timer_2
-    PR2 = 60000;               //sets the maximum count for Timer_2
+    PR2 = PR2_INITIAL_VALUE;               //sets the maximum count for Timer_2
     T2CONbits.TCS = 0;      //choose FCY as clock source for Timer_2
-    T2CONbits.TCKPS = 0x00; //sets the Timer_2 pre-scaler to 1
+    T2CONbits.TCKPS = 2;    //sets the Timer_2 pre-scaler to 1:8
     IFS0bits.T2IF = 0;      //clears Timer_2 interrupt flag
     _T2IE = 1;              //enables Timer_2 Interrupts
-    T2CONbits.TON = 1;    //turns Timer_2 OFF     
+    IPC1bits.T2IP=5;
+    T2CONbits.TON = 1;      //turns Timer_2 OFF     
     
 }
 
@@ -101,9 +115,10 @@ void TIMER3_config(){
     TMR3 = 0;               //resets Timer_3
     PR3 = MY_PR3;               //sets the maximum count for Timer_3
     T3CONbits.TCS = 0;      //choose FCY as clock source for Timer_3
-    T3CONbits.TCKPS = 0b11; //sets the Timer_3 pre-scaler to 1
-    IFS0bits.T3IF = 0;      //clears Timer_3 interrupt flag
+    T3CONbits.TCKPS = 0b00; //sets the Timer_3 pre-scaler to 1:2
+    IFS0bits.T3IF = 0;      //clears Timer_3 interrupt flagPR2_FINAL_VALUE
     _T3IE = 1;              //enables Timer_3 Interrupts
+    IPC1bits.T3IP=3;
     T3CONbits.TON = 1;    //turns Timer_3 OFF     
     
 }
@@ -115,9 +130,9 @@ void PWM_init(){
     PTCONbits.PTCKPS=0b00; //Source-Timer Prescaler (0,1,2,3) (1:1, 1:4, 1:16, 1:64)
     PTPER=MY_PTPER;        //max 32767
     PTMR=0;                //Set the timer to initial value 0
-    PDC1=PTPER;            //if PDC is PTPER dc=50%, PDC is double of duty-cycle
-    PDC2=PTPER/2;          //dc=25%
-    PDC3=PTPER+PTPER/2;    //dc=75%
+    PDC1=0;            //if PDC is PTPER dc=50%, PDC is double of duty-cycle
+    PDC2=0;          //dc=25%
+    PDC3=0;    //dc=75%
     PWMCON1bits.PEN1H=1;   //Enable PWMxH pins
     PWMCON1bits.PEN2H=1;
     PWMCON1bits.PEN3H=1;
@@ -151,7 +166,8 @@ void __attribute__((interrupt, auto_psv)) _PWMInterrupt( void )
 void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void){    
     
     IFS0bits.T2IF = 0;         
-    
+     //if(g_sector==0 && g_ang==0)
+         
     //Update PDC code
     setPDC(g_sector,g_ang);
     g_sector=(g_sector+(g_ang+g_angIncrement)/ANGLE_RESOLUTION)%6;        
@@ -162,33 +178,67 @@ void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void){
 
 void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void){    
     
-    IFS0bits.T3IF = 0;         
+    IFS0bits.T3IF = 0;      
     
-    static unsigned int pr2_old=0;
-    static float temp;
     
-    _LATF0=(!_LATF0); //Every interrupt toggle F0 bit, which connects to LED D6
-    _LATD3=(!_LATD3);
-    pr2_old=PR2;
-    temp=(PR2/1000.)*(PR2/1000.);
-    //PR2-=(unsigned int)((double)PR2*(double)PR2/(double)331456);
-    PR2-=(unsigned int)(1000000./g_inverse*temp);
-    if(pr2_old==PR2)
+   
+
+    g_truePR2-=(g_truePR2*g_truePR2/g_inverse);
+    
+    static int gm_first_update = 1,
+               //gm_second_update = 1,
+               prescaler_update = 1;
+    
+
+    
+
+        if(g_truePR2<PR2_FINAL_VALUE)
     {
-//        g_inverse=53084;
-//        PR3=41667;
-        _LATF1=1;
-    }
+        _T3IE = 0; //Disable aceleration
+        _LATF1=!_LATF1;
+        _LATD3=1; //Toggle D3/OC4 pin for viewing in osciloscope
+        //start_breaking();
+        //regen();
+    }    
+       
+    else{
     
+        PR2=(unsigned int)g_truePR2;
         
+        if(prescaler_update && g_truePR2 <= 719){
+            
+           T2CONbits.TCKPS = 0;
+           PR2=46079;
+           g_truePR2=46079.0;
+           prescaler_update=0;
+           g_inverse=NEXT_INVERSE_FACTOR/ACELERACAO;
+           _LATF1=!_LATF1;
+        }
+            
+    
+//            if(gm_second_update && g_truePR2 < 500.0 + 0.5)
+//    {
+//        g_m=GM_SECOND_JUMP;
+//        gm_second_update=0;
+//        _LATF0=!_LATF0; 
+//    }
+    
+        if(gm_first_update && !prescaler_update && g_truePR2 < 10000. +0.5 )
+    {
+        g_m=GM_FIRST_JUMP;
+        gm_first_update=0;
+        _LATF0=!_LATF0; 
+    }
+
+        }
     
 }
 
 void setPDC(int setor, int angB) {
 	static int tA, tB, t02, angA;
 	angA = LAST_TABLE_INDEX - angB;
-	tB = 2 * tab[angB]/g_m;
-	tA = 2 * tab[angA]/g_m;
+	tB = 2 * tab[angB]*g_m/10;
+	tA = 2 * tab[angA]*g_m/10;
 	t02 = (2*MY_PTPER - tA - tB)/2;
 
 	//switch
@@ -225,4 +275,73 @@ void setPDC(int setor, int angB) {
 	            //break;							
 	  }
 
+}
+
+
+void start_breaking(){
+    //Configure PWM pins
+    _T2IE = 0;
+    _T3IE = 0;
+    
+    _LATE2 = 0;  //L6234 En pins
+    _LATE0 = 0;
+    _LATE4 = 0; 
+    
+    PTCONbits.PTEN=0;
+    
+    PTPER=1800;  
+    PTCONbits.PTCKPS=0b10;
+    
+    PWMCON1bits.PEN1H=0;   //Set PWMxH pins as regular I/O
+    PWMCON1bits.PEN2H=0;
+    PWMCON1bits.PEN3H=0;
+    
+    _TRISE3 = 0;
+    _TRISE1 = 0;
+    _TRISE5 = 0;
+    
+    _LATE3 = 0;
+    _LATE1 = 0;
+    _LATE5 = 0; 
+    
+    PWMCON1bits.PEN1L=1;   //Enable PWMxL pins
+    PWMCON1bits.PEN2L=1;
+    PWMCON1bits.PEN3L=1;
+    
+    PDC1=PTPER;
+    PDC2=PTPER;
+    PDC3=PTPER;
+       
+    PTCONbits.PTEN=1;
+    
+}
+
+void regen(){
+    
+
+    _T3IE = 0;
+    
+    _LATE2 = 0;  //L6234 En pins
+    _LATE0 = 0;
+    _LATE4 = 0; 
+    
+    
+    
+    T2CONbits.TON=0;
+    TMR2=0;
+    PR2=50;
+    
+    T2CONbits.TCS = 0;
+    T2CONbits.TCKPS = 2;
+    IFS0bits.T2IF=0;
+    _T2IE = 0;
+    
+    
+    OC2RS=PR2/2;
+    OC2CONbits.OCM=6;
+    OC2R=PR2/2;
+    OC2CONbits.OCTSEL=0;
+    
+    T2CONbits.TON=1;
+    
 }
